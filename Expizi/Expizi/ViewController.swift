a077627bddb5978b2501435f9bd8380c537d8d47
Aditya M Naik <30904212+adi3452@users.noreply.github.com>
2022-04-07 13:53:25 -0400

    Add files via upload
    Expizi initial commit

---
+++ Expizi/Expizi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expizi
+//
+//  Created by ADITYA M NAIK on 2022-03-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
